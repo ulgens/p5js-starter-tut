872cc30eb5e92b83b76db66a26b92e49b14787a4
Ülgen Sarıkavak <ulgensrkvk@gmail.com>
2016-11-27 12:48:54 +0300

    Add step 2: Falling object

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 Code Examples:
 * Step 0: Initial screen
-* Step 1: Create objects with mouse click
-* Step 2: Define gravity
-* Step 3: Make objects follow the mouse
+* Step 1: Create shapes with mouse click
+* Step 2: Define gravity, on an object
+* Step 3: Create multiple falling objects with mouse click
 * Step 4: Make objects BOOOM when they catch the mouse
---
+++ step2/sketch.js
@@ -0,0 +1,38 @@
+// Define ball variable
+var ball;
+
+function setup() {
+    // Create the canvas: https://p5js.org/reference/#/p5/createCanvas
+    createCanvas(500, 500);
+    // Set backround color: https://p5js.org/reference/#/p5/background
+    background(200);
+    // Create new ball object
+    ball = new Ball();
+}
+
+function draw(){
+    // Clear background
+    background(200);
+
+    // Draw and move ball object
+    ball.display();
+    ball.move();
+}
+
+function Ball(){
+    this.pos = createVector(random(width), 0);
+    this.vel = createVector(0, 0);
+    this.acc = createVector(0, 9.8);
+
+    this.move = function() {
+        this.acc.normalize();
+        this.acc.mult(0.1);
+        this.vel.add(this.acc);
+        this.pos.add(this.vel);
+    }
+
+    this.display = function(){
+        fill(255);
+        ellipse(this.pos.x, this.pos.y, 50, 50);
+    }
+}
