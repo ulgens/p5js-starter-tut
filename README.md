a6ed55bbbec81375fe4be7763778574fd2fac163
Ülgen Sarıkavak <ulgensrkvk@gmail.com>
2016-11-27 12:14:35 +0300

    Add step 0: Initial screen

---
+++ README.md
@@ -0,0 +1,6 @@
+Code Examples:
+* Step 0: Initial screen
+* Step 1: Create objects with mouse click
+* Step 2: Define gravity
+* Step 3: Make objects follow the mouse
+* Step 4: Make objects BOOOM when they catch the mouse
---
+++ step0/sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+    // Create the canvas: https://p5js.org/reference/#/p5/createCanvas
+    createCanvas(500, 500);
+    // Set backround color: https://p5js.org/reference/#/p5/background
+    background(200);
+}
+
+function draw(){
+	// Nothing to draw yet.
+}
